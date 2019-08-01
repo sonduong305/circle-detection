3cbc307aa804310a5af6e68602394e7097df9e17
sonduong305 <sonduong305@gmail.com>
2019-08-01 17:19:52 +0700

    fisnish

---
+++ CircleF_contourArea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Drawing;
+using System.Xml.Serialization;
+using Emgu.CV.Structure;
+
+namespace EMU
+{
+    class CircleF_contourArea
+    {
+            public CircleF circle { get; set; }
+            public double contourArea { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@ using Emgu.CV.CvEnum;
 
 using AForge.Imaging;
 using AForge.Math;
+using EMU;
+
 namespace Emgu.CV
 {
     class Program
@@ -22,17 +24,17 @@ namespace Emgu.CV
         {
 
 
-            string path = "C:\\Users\\Blake\\Desktop\\opencv_thuy\\error\\New folder\\";
-            //string path = "C:\\Users\\Blake\\Desktop\\opencv_thuy\\error\\";
+            //string path = "C:\\Users\\Blake\\Desktop\\opencv_thuy\\error\\New folder\\";
+            string path = "C:\\Users\\Blake\\Desktop\\opencv_thuy\\error\\";
             string fileName = "DeskCamera2_DeskCamera2_18551.bmp";
             string resultPath = "C:\\result\\";
-            int estimatedRadius = 120;
-            List<CircleF> circles;
+            int estimatedRadius = 40;
+            List<CircleF_contourArea> circles;
             //Bitmap img;
             Image<Gray, Byte> img1,img;
 
             
-            foreach (string file in Directory.EnumerateFiles(path, "*.bmp"))
+            foreach (string file in Directory.EnumerateFiles(path, "*.jpg"))
             {
                 string filename = Path.GetFileName(file);
                 img1 = new Image<Gray, Byte>(file);
@@ -49,10 +51,10 @@ namespace Emgu.CV
                 //ProcessImage(img);
                 Console.WriteLine("found " + circles.Count + " circle(s) with your estimated radius = " + estimatedRadius );
                 Console.WriteLine("List of circles: ");
-                foreach (CircleF circle in circles)
+                foreach (CircleF_contourArea circle in circles)
                 {
-                    Console.WriteLine("\t [" + circle.Center + " \t R = " + circle.Radius + " \t score = " + Circle_score(circle, img)+" ]");
-                    img1.Draw(circle, new Gray(0), 3);
+                    Console.WriteLine("\t [" + circle.circle.Center + " \t R = " + circle.circle.Radius + " \t score = " + circle.contourArea / circle.circle.Area + " ]");
+                    img1.Draw(circle.circle, new Gray(0), 3);
                     
 
 
@@ -73,23 +75,21 @@ namespace Emgu.CV
             return kernel;
         }
         /// <summary>
-        /// Find circles in Image with estimated Radius
+        /// Find circles in Image with estimated Radius using contours
         /// </summary>
         /// <returns>
         /// The list of found circles.
         /// </returns>
         /// <param name="img">Source image with Image format.</param>
         /// <param name="estimatedRadius">Expected Radius.</param>
-        static List<CircleF> FindCircle_contours(Image<Gray, Byte> image, int estimatedRadius)
+        static List<CircleF_contourArea> FindCircle_contours(Image<Gray, Byte> image, int estimatedRadius)
         {
             
             Image<Gray, Byte> bilateral_filtered_image,edge_detected_image, dilated, eroded,img;
             img = image.Clone();
             bilateral_filtered_image = new Mat().ToImage<Gray,byte>();
             edge_detected_image = new Mat().ToImage<Gray, byte>();
-
-
-            //dilated = new Mat().ToImage<Gray, byte>();
+            dilated = img.Clone();
             eroded = new Mat().ToImage<Gray, byte>();
             Mat hierarchy = new Mat();
             CvInvoke.MorphologyEx(img, img, MorphOp.Close, ellipse_kernel_gen(13), new Point(-1, -1), 1, BorderType.Default, new MCvScalar());
@@ -101,8 +101,9 @@ namespace Emgu.CV
 
             CvInvoke.MorphologyEx(edge_detected_image, eroded, MorphOp.Close, ellipse_kernel_gen(11), new Point(-1, -1), 1, BorderType.Default, new MCvScalar());
             VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
-            CvInvoke.FindContours(eroded, contours, hierarchy, RetrType.Tree, ChainApproxMethod.ChainApproxSimple);
-            List<CircleF> circles = new List<CircleF>();
+            CvInvoke.FindContours(eroded, contours, hierarchy, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+            List<CircleF_contourArea> circles = new List<CircleF_contourArea>();
+            
             for (int i = 0; i < contours.Size; i++)
             {
                 using (VectorOfPoint contour = contours[i])
@@ -118,9 +119,13 @@ namespace Emgu.CV
                         CircleF circle = CvInvoke.MinEnclosingCircle(contour);
                         double circle_area = circle.Radius * circle.Radius * 3.14;
                         //Console.WriteLine("Circle: rect area : " + rect_area.ToString() + ", circle area: " + circle_area.ToString()+ " R = " +circle.Radius.ToString());
-                        if ((Math.Abs(rect_area - circle_area) < rect_area / 10) && (Math.Abs(Math.Sqrt(circle_area/3.14) - estimatedRadius) < 100))
+                        if ((Math.Abs(rect_area - circle_area) < rect_area / 10) && (Math.Abs(Math.Sqrt(circle_area / 3.14) - estimatedRadius) < 30)) 
                         {
-                            circles.Add(circle);
+                            CircleF_contourArea temp = new CircleF_contourArea();
+                            temp.circle = circle;
+                            temp.contourArea = CvInvoke.ContourArea(contour);
+                            circles.Add(temp);
+                            //Console.WriteLine("Contour area: " + CvInvoke.ContourArea(contour));
                             
                         }
                     }
@@ -128,11 +133,11 @@ namespace Emgu.CV
                 }
             }
 
- 
+
             //CvInvoke.Imshow("bilateral_filtered_image", bilateral_filtered_image);
             //CvInvoke.Imshow("edge", edge_detected_image);
             //CvInvoke.Imshow("img", img);
-            //CvInvoke.Imshow("eroded", eroded);
+            //CvInvoke.Imshow("circle contour", dilated);
 
             return circles;
         }
@@ -192,23 +197,31 @@ namespace Emgu.CV
             return 0;
         }
 
-        static double Circle_score(CircleF circle, Image<Gray,byte> image)
+        static double Circle_score(CircleF_contourArea circle, Image<Gray,byte> image)
         {
             Image<Gray, byte> gradient = image.Clone();
+            Image<Gray, byte> gradient_cropped = image.Clone();
             Image<Gray, byte> img = image.Clone();
-            CvInvoke.MedianBlur(img, img, 9);
+            CvInvoke.MedianBlur(img, img, 13);
             double score = 0;
 
             Image<Gray, byte> mask = new Image<Gray, byte>(img.Width, img.Height);
-            CvInvoke.Circle(mask, Point.Round(circle.Center), (int) circle.Radius, new MCvScalar(255, 255, 255), -1);
+            CvInvoke.Circle(mask, Point.Round(circle.circle.Center), (int) circle.circle.Radius, new MCvScalar(255, 255, 255), -1);
             Image<Gray, byte> mask_white = mask.Clone();
+            Image<Gray, byte> mask_small = mask.Clone();
+            CvInvoke.Circle(mask_small, Point.Round(circle.circle.Center), (int)circle.circle.Radius + 2, new MCvScalar(255), -1);
+            CvInvoke.Circle(mask_small, Point.Round(circle.circle.Center), (int)circle.circle.Radius - 2 , new MCvScalar(0), -1);
             Image<Gray, byte> dest = new Image<Gray, byte>(img.Width, img.Height);
             //-1 is to fill the area
             Mat cropped = new Mat();
             CvInvoke.BitwiseAnd(img, mask, cropped);
-            //CvInvoke.Laplacian(img, gradient, DepthType.Default);
-            CvInvoke.Circle(mask, Point.Round(circle.Center), (int)circle.Radius, new MCvScalar(CvInvoke.Mean(cropped, mask).ToArray()[0]), -1);
+            
+            CvInvoke.Laplacian(img, gradient, DepthType.Default);
+            CvInvoke.BitwiseAnd(gradient, mask_small, gradient_cropped);
+            
+            CvInvoke.Circle(mask, Point.Round(circle.circle.Center), (int)circle.circle.Radius, new MCvScalar(CvInvoke.Mean(cropped, mask).ToArray()[0]), -1);
             CvInvoke.AbsDiff(mask, cropped, mask);
+            CvInvoke.Canny(cropped, cropped, 25, 25);
             score = CvInvoke.Sum(cropped).ToArray()[0] / CvInvoke.Sum(mask_white).ToArray()[0];
             CvInvoke.Imshow("cropped", cropped);
             //CvInvoke.WaitKey(0);
